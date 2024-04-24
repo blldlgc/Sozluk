06620bccd85188075064880ca6a1b840768444c9
Bilal Dalgic <119047452+blldlgc@users.noreply.github.com>
2024-04-24 14:37:18 +0300

    Kelime girme ekranında düzeltmeler yapıldı

--- Sozluk/Pages/WordAddingPage.xaml.cs
+++ Sozluk/Pages/WordAddingPage.xaml.cs
@@ -11,4 +11,37 @@ public partial class WordAddingPage : ContentPage
 	{
         await App.Current.MainPage.Navigation.PopModalAsync();
     }
+
+    private async void SaveBtnClicked(object sender, EventArgs e)
+    {
+        await App.Current.MainPage.Navigation.PopModalAsync();
+    }
+
+    private async void PhotoBtnClicked(object sender, EventArgs e)
+    {
+        try
+        {
+            var result = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
+            {
+                Title = "Please pick a photo"
+            });
+            if (result != null)s
+            {
+                // Seçilen fotoğrafın işlenmesi
+                var stream = await result.OpenReadAsync();
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    await stream.CopyToAsync(ms);
+                    byte[] photoData = ms.ToArray();
+                    // Burada fotoğraf verilerini kullanabilirsiniz
+                    var imageSource = ImageSource.FromStream(() => new MemoryStream(photoData));
+                    selectedImage.Source = imageSource;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            await App.Current.MainPage.DisplayAlert("Error", ex.Message, "OK");
+        }
+    }
 }
